005f990b4aa7ba434fe86a58b8036a3b5bb639bc
fratertamas <fratertomi@gmail.com>
2024-05-19 13:25:32 +0200

    Fix checkstyle issues

--- src/main/java/nye/progkor/controller/CompetitionController.java
+++ src/main/java/nye/progkor/controller/CompetitionController.java
@@ -1,13 +1,21 @@
 package nye.progkor.controller;
 
+import java.util.List;
+
 import nye.progkor.model.Competition;
 import nye.progkor.model.Performer;
 import nye.progkor.service.CompetitionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/competitions")
